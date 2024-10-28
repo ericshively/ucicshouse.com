3db449c717d8e3ecf5d43619e35a053e48ae0995
Eric Shively <eshively@uci.edu>
2024-10-27 22:14:00 -0700

    Update readme, less meteors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Meteors from "@/components/ui/meteors";
 
 export default function RootPage() {
   return (
-    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
-      <Meteors number={1000} />
+    <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+      <Meteors number={300} />
       <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
         ICS HOUSE!!!
       </span>
